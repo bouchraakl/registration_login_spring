56553d88a99910dded39b46202dfb557c23b4e31
Bouchra Akl <bushraakl1234@gmail.com>
2023-05-30 18:55:38 -0300

    Initial commit

---
+++ src/test/java/com/project/registerSpring/RegisterSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.registerSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
