1ed4aa58a2959e42abec26ef4cb7adf25788e700
Bouchra Akl <bushraakl1234@gmail.com>
2023-05-30 19:01:55 -0300

    UserService structured

---
+++ src/main/java/com/project/registerSpring/service/UserService.java
@@ -0,0 +1,27 @@
+package com.project.registerSpring.service;
+
+
+import com.project.registerSpring.model.User;
+import com.project.registerSpring.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    public UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("email does not exist.");
+        }
+        return user;
+    }
+}
