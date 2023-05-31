d995bc950684156d0a8b4d20ff182cafcc0768f4
Bouchra Akl <bushraakl1234@gmail.com>
2023-05-31 19:03:48 -0300

    RegisterService.register base started

--- src/main/java/com/project/registerSpring/service/RegisterService.java
+++ src/main/java/com/project/registerSpring/service/RegisterService.java
@@ -1,12 +1,25 @@
 package com.project.registerSpring.service;
 
-import com.project.registerSpring.requests.RegisterRequest;
+import com.project.registerSpring.model.Role;
+import com.project.registerSpring.model.User;
+import com.project.registerSpring.requests.RegisterDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegisterService {
 
-    public String register(RegisterRequest request) {
-        return "works";
+    @Autowired
+    public UserService userService;
+
+    public void register(RegisterDTO request) {
+
+        // TODO: 5/31/2023 INITIATE NEW USER
+        userService.signUpOperation(new User(request.getFirstName()
+                ,request.getLastName(),
+                request.getEmail(),
+                request.getPassword(),
+                Role.USER));
+
     }
 }
